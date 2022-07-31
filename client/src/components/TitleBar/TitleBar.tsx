671aef467c723452a5bb1a115dd7add25065aefe
jake-the-cake <askjake331@gmail.com>
2022-07-31 06:55:06 -0400

    more logo work

--- client/src/components/TitleBar/TitleBar.tsx
+++ client/src/components/TitleBar/TitleBar.tsx
@@ -5,7 +5,7 @@ export default () => {
   return (
     <div className='titlebar__container'>
       <div className="titlebar__logo--box">
-        <div className="titlebar__logo--tree tree1">
+        <div className="titlebar__logo--tree">
           <div className="titlebar__logo--tree-top tri"></div>
           <div className="titlebar__logo--tree-middle tri"></div>
           <div className="titlebar__logo--tree-bottom tri"></div>
@@ -19,12 +19,10 @@ export default () => {
           <div className="titlebar__logo--tree-trunk"></div>
         </div>
 
-        <div className="titlebar__logo--tree tree3">
-          <div className="titlebar__logo--tree-top tri"></div>
-          <div className="titlebar__logo--tree-middle tri"></div>
-          <div className="titlebar__logo--tree-bottom tri"></div>
-          <div className="titlebar__logo--tree-trunk"></div>
-        </div>
+        <div className="titlebar__logo--title">
+          <span className='logo-title-top'>Take<br />A</span>
+          <span className='logo-title-bottom'>HIKE</span></div>
+      
       </div>
       <div className="titlebar__image--box">Photo</div>
     </div>
